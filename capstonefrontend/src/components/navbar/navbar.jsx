7e66de88b1349bb8cb94bdde16a9537c61fa314c
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-08-17 18:47:43 -0400

    added Link in navbar

--- capstonefrontend/src/components/navbar/navbar.jsx
+++ capstonefrontend/src/components/navbar/navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './navbar.css'
+import { Link } from 'react-router-dom'
 
 export default function NavBar() {
     return (
         <nav>
             <h3> Logo </h3>
             <ul className="navlinks">
-                <li>Log Out</li>
-                <li>Closet</li>
-                <li>Profile</li>
+            <Link to='/home'>
+            <li>Log Out</li>
+            </Link>
+            <Link to='closet'>
+            <li>Closet</li>
+            </Link> 
+            <Link to='profilepage'>
+            <li>Profile</li>
+            </Link> 
+                
+               
             </ul>
         </nav>
             
