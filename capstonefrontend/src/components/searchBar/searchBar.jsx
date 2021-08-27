5e50d7d26ebdf8b980227e9e3f11f50ce9c064b4
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-08-27 15:26:31 -0400

    finished searchBar.jsx

--- capstonefrontend/src/components/searchBar/searchBar.jsx
+++ capstonefrontend/src/components/searchBar/searchBar.jsx
@@ -2,20 +2,30 @@ import React, { useState } from 'react'
 import Data from '../homePage/clothes.json'
 
 export default function SearchBar() {
-    const [searchItem, setSearchItem] = useState('string')
+    const [searchItem, setSearchItem] = useState('')
     return (
         <div>
-            <input className="search-bar" type="text" placeholder="Search..." onChange={e => {setSearchItem(e.target.value)}}/>
+            <input className="search-bar" type="text" placeholder="Search..." onChange={event => {setSearchItem(event.target.value)}}/>
             {Data.filter((value) => {
             if(searchItem === ""){
-                return value
+                return ("")
             } else if (value.clotheName.toLowerCase().includes(searchItem.toLowerCase())){
                 return value
+                
             }
+        }).map((value, key)=>{
+            return(
+                <div className="" key={key}>
+                    <p>{value.clotheName}</p>
+                </div>
+
+            );
         })}
+        
         </div>
+    )
     
        
-    )
+    
     
 }
